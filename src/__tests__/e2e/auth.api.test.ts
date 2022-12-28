1456d2ead31f852d0501c4804fa45ed8dc8f44fb
VadimMaleev <maleeffset@yandex.by>
2022-12-28 13:12:44 +0300

    add tests logic for password recovery endpoints

--- src/__tests__/e2e/auth.api.test.ts
+++ src/__tests__/e2e/auth.api.test.ts
@@ -3,6 +3,7 @@ import request from "supertest";
 import {app} from "../../app";
 import {runDb} from "../../db";
 import * as cheerio from 'cheerio'
+import {randomUUID} from "crypto";
 
 describe('auth tests',() => {
     jest.setTimeout(1000 * 60 * 3); // 3minutes
@@ -14,6 +15,8 @@ describe('auth tests',() => {
     const loginUri = '/auth/login'
     const registrationUri = '/auth/registration'
     const confirmationUri = '/auth/registration-confirmation'
+    const passwordRecoveryUri = '/auth/password-recovery'
+    const newPasswordUri = '/auth/new-password'
     const aboutMeUri = '/auth/me'
 
     const validUser = {
@@ -28,6 +31,8 @@ describe('auth tests',() => {
     }
 
     let confirmationCode = ''
+    let recoveryCode = ''
+    let newPassword = 'newPassword'
     let token = ''
 
 
@@ -105,6 +110,77 @@ describe('auth tests',() => {
             token = response.body.accessToken
             expect(response.status).toBe(200)
             expect(response.body.accessToken).toBeDefined()
+        })
+
+        it('should return user info', async () => {
+            const response = await request(server).get(aboutMeUri)
+                .set("Authorization", "Bearer " + token)
+
+            expect(response).toBeDefined()
+            expect(response.status).toBe(200)
+            expect(response.body.email).toBe(validUser.email)
+            expect(response.body.login).toBe(validUser.login)
+        })
+    })
+
+    describe ('password-recovery', () => {
+        it('should return Error 400', async () => {
+            const response = await request(server).post(passwordRecoveryUri).send({})
+            expect(response.status).toBe(400)
+        })
+
+        it ('should return 204 and send email', async () => {
+            const response = await request(server).post(passwordRecoveryUri).send({email: validUser.email})
+
+            const mailBox: MailBoxImap = expect.getState().mailBox;
+            const email = await mailBox.waitNewMessage(2);
+            const html: any = await mailBox.getMessageHtml(email);
+            expect(html).not.toBeNull();
+            const link = cheerio.load(html).root().find('a').attr('href');
+            recoveryCode = link.split('?')[1].split('=')[1];
+            expect(recoveryCode).not.toBeNull();
+            expect(recoveryCode).not.toBeUndefined();
+
+            expect(response.status).toBe(204)
+        })
+    })
+
+    describe ('set new-password', () => {
+        it('should return Error400 -> password invalid', async () => {
+            const response = await request(server).post(newPasswordUri).send({newPassword: '', recoveryCode: recoveryCode})
+            expect (response.status).toBe(400)
+            expect (response.body.errorsMessages.length).toBe(1)
+        })
+
+        it('should return Error400 -> code invalid', async () => {
+            const response = await request(server).post(newPasswordUri).send({
+                newPassword: newPassword,
+                recoveryCode: randomUUID()
+            })
+            expect(response.status).toBe(400)
+            expect(response.body.errorsMessages.length).toBe(1)
+        })
+
+        it ('it should return 204 status', async () => {
+            const response = await request(server).post(newPasswordUri).send({newPassword: newPassword, recoveryCode: recoveryCode})
+            expect (response.status).toBe(204)
+        })
+    })
+
+    describe ('try to login with new Password', () => {
+        it ('should return Error because old password', async () => {
+            const response = await request(server).post(loginUri).send(loginValidUser)
+
+            expect(response.status).toBe(401)
+        })
+
+        it ('should return token', async () => {
+            const response = await request(server).post(loginUri).send({loginOrEmail: loginValidUser.loginOrEmail, password: newPassword})
+
+            token = response.body.accessToken
+
+            expect(response.body.accessToken).toBeDefined()
+            expect(response.status).toBe(200)
         })
 
         it('should return user info', async () => {
